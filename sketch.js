0ec1509134c047c784935f822508493fce528124
Hemant <109237389+hemantjain21@users.noreply.github.com>
2024-10-13 07:38:38 +0530

    Etch a sketch designed

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const maxWidth = 960;
+let div = [];
+let columns = 16, rows = 16;
+
+let container = document.querySelector(".container");
+
+//set container border
+container.setAttribute("style", `width: ${maxWidth}px`);
+
+let boxWidth = (maxWidth/columns).toFixed(0);
+    for(let i = 1; i <= rows*columns; i++){
+        div[i] = document.createElement("div");
+        div[i].setAttribute("style", `height: ${boxWidth}px; width: ${boxWidth}px; border: 1px solid red`);
+        container.appendChild(div[i]);
+        
+        div[i].addEventListener("mouseover", (e) => {
+            div[i].style.background = "green";
+        });
+    }
+
+
+//prompting row and column values
+let grids = document.querySelector(".grids");
+
+grids.addEventListener("click", (e) => {
+
+    while(container.lastChild){
+        container.removeChild(container.lastChild);
+    }
+
+    columns = +prompt("Enter number of columns ");
+    if(!(columns > 0 && columns <= 100)){
+        alert("Please enter a number between 1 to 100");
+        columns = +prompt("Enter number of columns ");
+    }
+
+    rows = +prompt("Enter number of rows: ");
+    if(!(rows > 0 && rows <= 100)){
+        alert("Please enter a number between 1 to 100");
+        rows = +prompt("Enter number of rows ");
+    }
+
+    boxWidth = (maxWidth/columns).toFixed(0);
+    for(let i = 1; i <= rows*columns; i++){
+        div[i] = document.createElement("div");
+        div[i].setAttribute("style", `height: ${boxWidth}px; width: ${boxWidth}px; border: 1px solid red`);
+        container.appendChild(div[i]);
+        
+        div[i].addEventListener("mouseover", (e) => {
+            div[i].style.background = "green";
+        });
+    }
+});
